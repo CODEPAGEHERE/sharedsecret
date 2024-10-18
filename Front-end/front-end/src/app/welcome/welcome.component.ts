5f90c4f15fafc9fbb651bfdb2107d3ede76efbcd
codepageme <codepage.me@gmail.com>
2024-10-18 02:07:52 -0700

    two o seven

--- Front-end/front-end/src/app/welcome/welcome.component.ts
+++ Front-end/front-end/src/app/welcome/welcome.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
+  imports: [RouterModule], // Add RouterModule here
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
